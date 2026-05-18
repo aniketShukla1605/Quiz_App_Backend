31060d26ab455df5b264bcea2186959ed4a6b2ec
aniketShukla <aniketshukla1605@gmail.com>
2026-05-18 20:36:33 +0530

    User GenericJacksonJsonRedisSerializer

--- quiz/src/main/java/com/microservice/quiz/config/CacheConfig.java
+++ quiz/src/main/java/com/microservice/quiz/config/CacheConfig.java
@@ -6,21 +6,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
+import tools.jackson.databind.ObjectMapper;
 //import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
 
 @Configuration
 public class CacheConfig {
+    ObjectMapper objectMapper = new ObjectMapper();
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory factory) {
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(10))
                 .serializeValuesWith(
                         RedisSerializationContext.SerializationPair
-                                .fromSerializer(new GenericJackson2JsonRedisSerializer())
+                                .fromSerializer(new GenericJacksonJsonRedisSerializer(objectMapper))
                 );
 
         return RedisCacheManager.builder(factory)
