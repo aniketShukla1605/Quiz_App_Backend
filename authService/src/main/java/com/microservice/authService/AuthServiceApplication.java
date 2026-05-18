93366e17961fad5ce52a9b9b4d5539aac2624055
aniketShukla <aniketshukla1605@gmail.com>
2026-05-18 13:17:27 +0530

    Redis Configuration

--- authService/src/main/java/com/microservice/authService/AuthServiceApplication.java
+++ authService/src/main/java/com/microservice/authService/AuthServiceApplication.java
@@ -2,8 +2,10 @@ package com.microservice.authService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
---
+++ authService/src/main/java/com/microservice/authService/config/CacheConfig.java
@@ -0,0 +1,35 @@
+package com.microservice.authService.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+public class CacheConfig {
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory factory) {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10))
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair
+                                .fromSerializer(new StringRedisSerializer())
+                );
+
+        return RedisCacheManager.builder(factory)
+                .cacheDefaults(config)
+                .withCacheConfiguration("quiz",
+                        config.entryTtl(Duration.ofMinutes(30)))   // quizzes change rarely
+                .withCacheConfiguration("questions",
+                        config.entryTtl(Duration.ofMinutes(30)))
+                .withCacheConfiguration("resultHistory",
+                        config.entryTtl(Duration.ofMinutes(5)))    // shorter TTL, changes after each attempt
+                .build();
+    }
+}
